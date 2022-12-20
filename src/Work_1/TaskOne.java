ff3a83772151a9c9b8052a948cdf082be0d62bb8
tenggys <levgood21@yandex.ru>
2022-12-20 22:59:28 +0300

    Домашняя работа №1

---
+++ src/Work_1/TaskOne.java
@@ -0,0 +1,34 @@
+//Реализуйте 3 метода, чтобы в каждом из них получить разные исключения
+
+package Work_1;
+
+public class TaskOne {
+    private static int secondNum;
+    private static int firstNum;
+
+    public static void main(String[] args) {
+        String[] sArray = {"first str", null, "third str"};
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+        division(10, 0);
+        printValueByIndex(array, 12);
+        printStringArray(sArray);
+    }
+
+    private static void division(double firstNum, double secondNum) {
+        if(secondNum ==  0) throw new ArithmeticException("Деление на ноль не возможно!");
+        double result = firstNum/secondNum;
+        System.out.printf("Деление %f на %f равен %f.\n", firstNum, secondNum, result);
+    }
+
+    public static void printValueByIndex(int[] array, int index){
+        if(index > array.length-1) throw new ArrayIndexOutOfBoundsException("Элемента под таким индексом не существует");
+        else System.out.printf("Элемент с индексом %d равен %d.\n",index, array[index]);
+    }
+
+    public static void printStringArray(String[] sArray) {
+        for (String item : sArray) {
+            if (item == null) throw new NullPointerException("Элемента нет");
+            System.out.println(item);
+        }
+    }
+}
---
+++ src/Work_1/TaskTwo.java
@@ -0,0 +1,15 @@
+package Work_1;
+
+public class TaskTwo {
+
+    public static int sum2d(String[][] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < 5; j++) {
+                int val = Integer.parseInt(arr[i][j]);
+                sum += val;
+            }
+        }
+        return sum;
+    }
+}
